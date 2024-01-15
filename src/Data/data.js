6f1564ab6901aa66f5505901de7f659e82154820
BenjaminBoisedu <benjaminboisedu354@gmail.com>
2024-01-15 11:18:56 +0100

    Data

---
+++ src/Data/data.js
@@ -0,0 +1,17 @@
+export const myTasks = [
+  {
+    id: 1,
+    task: "Learn React",
+    done: false,
+  },
+  {
+    id: 2,
+    task: "Learn React",
+    done: false,
+  },
+  {
+    id: 3,
+    task: "Learn React",
+    done: false,
+  },
+];
