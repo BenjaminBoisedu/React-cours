ecc47f582b2d993d86ee8cb9e3dd5746cd5181a1
BenjaminBoisedu <benjaminboisedu354@gmail.com>
2024-01-15 11:08:28 +0100

    Test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
 
 function App() {
+  const name = "World";
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Hello {name}!</h1>
     </div>
   );
 }
