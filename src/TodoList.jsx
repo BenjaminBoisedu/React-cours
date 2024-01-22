94c6239bb1122869c153d32956f43f6133c55449
BenjaminBoisedu <benjaminboisedu354@gmail.com>
2024-01-22 09:12:21 +0100

    Sorted Todos

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -3,10 +3,14 @@ import React from "react";
 import "./todolist.css";
 
 export function TodoList({ todos, toggleTodo, deleteTodo }) {
+  const SortedTodos = todos.sort((a, b) => {
+    return Number(a.completed) - Number(b.completed);
+  });
+
   return (
     <ul className="list">
       {todos.length === 0 && "No todos to display"}
-      {todos.map((todo) => {
+      {SortedTodos.map((todo) => {
         return (
           <TodoItem
             {...todo}
